e55116636c164ce7147f016d8147bdcab446dc09
HP <umar@immersiveinfotech.com>
2023-04-21 14:36:31 +0530

    umar

--- lib/app/modules/Splash/controllers/splash_controller.dart
+++ lib/app/modules/Splash/controllers/splash_controller.dart
@@ -1,4 +1,3 @@
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
